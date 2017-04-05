a84ec04e41cf60075b8c5f89eca917503d492b51
Kevin Hubbard <dgkallday98@gmail.com>
2017-04-04 21:24:08 -0400

    added moment.js and .gitingnore

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
 	localStorage.clear();
+	
+
+	//initialize firebase
+	var config = {
+	    apiKey: "AIzaSyCLAE-khTunF6AO87jASrMRnGlo3_FB278",
+	    authDomain: "train-scheduler-app.firebaseapp.com",
+	    databaseURL: "https://train-scheduler-app.firebaseio.com",
+	    storageBucket: "train-scheduler-app.appspot.com",
+	    messagingSenderId: "651649548981"
+  	};
+
+
+ 	 firebase.initializeApp(config);
+ 	 var database = firebase.database();
+
+
+
+
+
 
 	$(document).on('click', '.btn', function() {
 		event.preventDefault();
@@ -12,19 +31,39 @@ $(document).ready(function() {
 		var $frequency = $('#frequency').val().trim();
 		var $table = $('.table');
 
-		console.log($train+$destination+$time+$frequency);
 
 		var train = {name:$train, destination:$destination, time:$time, frequency:$frequency};
 		console.log(train.name+train.destination+train.time+train.frequency);
 
 		localStorage.setItem('train', JSON.stringify(train));
 
-		$table.append('<tr><td>'+$train+'</td><td>'+$destination+'</td><td>'+ $time+'</td><td>'+ $frequency+'</td></tr>');
-		document.append($table);
 
+		console.log(JSON.stringify(moment().hour($time)));
+
+
+
+
+		database.ref('/trains ' + train.name).push(train);
+
+
+
+
+
+		$table.append('<tr><td>'+$train+'</td><td>'+$destination+'</td><td>'+ $time+'</td><td>'+ $frequency+'</td></tr>');
+	
+
+		clearKeys();
+
+
+	});
+
+
+	function clearKeys(){
+		console.log('is this being called??');
 		$('#train').val('');
 		$('#destination').val('');
 		$('#time').val('');
 		$('#frequency').val('');
-	});
+	}
+
 });
